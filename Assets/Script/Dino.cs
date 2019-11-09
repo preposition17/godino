742bb3148400bf63ae6456c0652223c0a87422ce
Vildan Safin <vildan.safin.99@gmail.com>
2019-11-09 20:07:09 +0500

    :zap: Added jump the left and right

--- Assets/Script/Dino.cs
+++ Assets/Script/Dino.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.iOS;
 
 public class Dino : MonoBehaviour
 {
@@ -17,19 +18,34 @@ public class Dino : MonoBehaviour
     
     private void Update(){
         DinoMoveForward();
-        DinoGravity();
-    }
-
-    private void DinoMoveForward(){
-        transform.Translate(direction*Time.deltaTime);
-        if(rb.velocity.y < 0){
-            rb.AddForce(Vector3.down * 10 * rb.mass);
+        DinoJump();
+        // Sometimes dino fails underground, i hope it helps, but not sure
+        if(transform.position.y < 0.49f){
+            transform.position = new Vector3(transform.position.x, 0.5f, transform.position.z);
         }
     }
-
-    public void DinoGravity(){
-        if(Input.GetButtonDown("Jump") && transform.position.y <= 0.5){
-            rb.AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+    private void DinoMoveForward(){
+        transform.Translate(direction*Time.deltaTime);
+        // When Dino is landing, we accelerate landing
+        if(rb.velocity.y < 0){
+            rb.AddForce(Vector3.down * 50 * rb.mass);
+        }
+    }
+    private void DinoJump(){
+        // If dino grounded and space button is down, then dino jumps
+        if(Input.GetMouseButtonDown(0)){    
+            if(Input.mousePosition.x > 230.0f && Input.mousePosition.x < 490.0f && transform.position.y <= 0.5){
+                // Jump
+                rb.AddForce(new Vector3(0,5,0), ForceMode.Impulse);
+            }
+            else if(Input.mousePosition.x <= 230.0f && transform.position.y <= 0.5){
+                // Jump to the left
+                rb.AddForce(new Vector3(0,5,1), ForceMode.Impulse);
+            }
+            else if(Input.mousePosition.x >= 490.0f && transform.position.y <= 0.5){
+                // Jump to the right
+                rb.AddForce(new Vector3(0,5,-1), ForceMode.Impulse);
+            }
         }
     }
 }
