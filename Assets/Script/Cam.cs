f8ba0fab548ed3fa05cd0aa17e861b394860f6e3
Vildan Safin <vildan.safin.99@gmail.com>
2019-11-09 12:19:35 +0500

    :tada: Started to make main machanics

---
+++ Assets/Script/Cam.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cam : MonoBehaviour
+{
+    private Vector3 dir;
+    void Start()
+    {
+        dir = Vector3.zero;
+        dir.x = 0.5f*Mathf.Cos(transform.eulerAngles.y);
+        dir.z = (0.5f*(Mathf.Sin(90 - transform.eulerAngles.y))/Mathf.Sin(transform.eulerAngles.x));
+        dir.y = (0.5f*(Mathf.Sin(90 - transform.eulerAngles.y))/Mathf.Cos(transform.eulerAngles.y));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(dir*Time.deltaTime);
+    }
+}
---
+++ Assets/Script/Dino.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dino : MonoBehaviour
+{
+    private float speed = 1f;
+    public float jumpPower = 10.0f;
+    private float gravityF;
+    public Rigidbody rb;
+    private Vector3 direction;
+    private void Start(){
+        rb = GetComponent<Rigidbody>();
+        direction = Vector3.zero;
+        direction.x = speed;
+    }
+    
+    private void Update(){
+        DinoMoveForward();
+        DinoGravity();
+    }
+
+    private void DinoMoveForward(){
+        transform.Translate(direction*Time.deltaTime);
+    }
+
+    public void DinoGravity(){
+        if(Input.GetButtonDown("Jump") && transform.position.y <= 0.5){
+            rb.AddForce(new Vector3(0,10,0), ForceMode.Impulse);
+        }
+    }
+}
